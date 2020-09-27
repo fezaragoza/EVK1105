da56bf6d798beb3865b4fca72a2e6585385e9e3c
Fernando Zaragoza <fer.zm@outlook.com>
2020-09-27 17:42:16 -0500

    Cleanup ExerciseA, working.

--- Source/Project1/Project1/src/main.c
+++ Source/Project1/Project1/src/main.c
@@ -40,6 +40,8 @@ typedef enum{
 	OR_AND
 } state_machine_t;
 
+void exercise_A(void);
+
 int main (void)
 {
 
@@ -50,6 +52,13 @@ int main (void)
 	/* Insert application code here, after the board has been initialized. */
 	
 	// Exercise A)
+	exercise_A();
+	
+		
+}
+
+void exercise_A(void)
+{
 	uint8_t count = 0, position = 0;
 	
 	// Configure LEDS and QT
@@ -64,106 +73,96 @@ int main (void)
 	// Poll
 	while (1)
 	{
-		static uint8_t num = 0; // Cannot have var declaration in the middle of cases. To workaround, put stuff inside case in a new scope {}
+		static uint8_t num = 0; // Cannot have variable declaration in the middle of cases. To workaround, put stuff inside case in a new scope {}
 		switch(state)
 		{
 			case ENTER_NUM:
-				poll_qt_button(&qt->button_s._left, QT_PRESSED);
-				poll_qt_button(&qt->button_s._right, QT_PRESSED);
-				poll_qt_button(&qt->button_s._enter, QT_PRESSED);
-				if (qt->button_s._left.active)
+			poll_qt_button(&qt->button_s._left, QT_PRESSED);
+			poll_qt_button(&qt->button_s._right, QT_PRESSED);
+			poll_qt_button(&qt->button_s._enter, QT_PRESSED);
+			if (qt->button_s._left.active)
+			{
+				if (position < 4)
 				{
-					if (position < 4)
-					{
-						position++;
-						num = evkl[count].ledx;
-						num |= 1 << (position - 1);
-						// Update LEDs
-						set_ledx_num(num);
-					}
-					
-				}
-				else if (qt->button_s._right.active)
-				{
+					position++;
 					num = evkl[count].ledx;
-					if (position > 0)
-					{
-						--position;
-						if ((position - 1) >= 0)
-							num |= 1 << (position - 1);	
-					}
+					num |= 1 << (position - 1);
 					// Update LEDs
 					set_ledx_num(num);
 				}
-			
-				if (qt->button_s._enter.active)
-				{
-					if (position > 0)
-					{
-						// Entered a number, store it.
-						evkl[count].ledx = num;
-						// Reset position to 0
-						position = 0;
-						// Update LEDs
-						set_ledx_num(evkl[count].ledx);
-					}
-					else
-					{
-						// Ready for the current number
-						num = 0;
-						// Reset position to 0
-						position = 0;
-						//Update LEDS
-						set_ledx_num(num);
-						if (++count == NUMBER)
-						{
-							// Ready
-							// Change state
-							state = OR_AND;
-						}
-					}
-				}
-				break;
-			case OR_AND:
-				poll_qt_button(&qt->button_s._up, QT_PRESSED);
-				poll_qt_button(&qt->button_s._down, QT_PRESSED);
-				poll_qt_button(&qt->button_s._enter, QT_PRESSED);
-				if (qt->button_s._down.active)
-				{
-					num = evkl[0].ledx | evkl[1].ledx | evkl[2].ledx | evkl[3].ledx;
-					set_ledx_num(num);
-				}
-				else if (qt->button_s._up.active)
-				{
-					num = evkl[0].ledx & evkl[1].ledx & evkl[2].ledx & evkl[3].ledx;
-					set_ledx_num(num);
-				}
 				
-				if (qt->button_s._enter.active)
+			}
+			else if (qt->button_s._right.active)
+			{
+				num = evkl[count].ledx;
+				if (position > 0)
 				{
-					// LED routine
-					state = ENTER_NUM;
-					// Reset all to 0
-					num = 0;
-					count = 0;
-					position = 0;
-					memset(&evkl, 0, sizeof(evkl));
-					// Update LEDS
-					set_ledx_num(num);
+					--position;
+					if ((position - 1) >= 0)
+					num |= 1 << (position - 1);
 				}
-				break;
+				// Update LEDs
+				set_ledx_num(num);
+			}
+			
+			if (qt->button_s._enter.active)
+			{
+				if (position > 0)
+				{
+					// Entered a number, store it.
+					evkl[count].ledx = num;
+					// Reset position to 0
+					position = 0;
+					// Update LEDs
+					set_ledx_num(evkl[count].ledx);
+				}
+				else
+				{
+					// Ready for the current number
+					num = 0;
+					// Reset position to 0
+					position = 0;
+					//Update LEDS
+					set_ledx_num(num);
+					if (++count == NUMBER)
+					{
+						// Ready, change state
+						state = OR_AND;
+					}
+				}
+			}
+			break;
+			case OR_AND:
+			poll_qt_button(&qt->button_s._up, QT_PRESSED);
+			poll_qt_button(&qt->button_s._down, QT_PRESSED);
+			poll_qt_button(&qt->button_s._enter, QT_PRESSED);
+			if (qt->button_s._down.active)
+			{
+				num = evkl[0].ledx | evkl[1].ledx | evkl[2].ledx | evkl[3].ledx;
+				set_ledx_num(num);
+			}
+			else if (qt->button_s._up.active)
+			{
+				num = evkl[0].ledx & evkl[1].ledx & evkl[2].ledx & evkl[3].ledx;
+				set_ledx_num(num);
+			}
+			
+			if (qt->button_s._enter.active)
+			{
+				// LED routine
+				state = ENTER_NUM;
+				// Reset all to 0
+				num = 0;
+				count = 0;
+				position = 0;
+				memset(&evkl, 0, sizeof(evkl));
+				// Update LEDS
+				set_ledx_num(num);
+			}
+			break;
 			default:
-				// Nothing to do in default case.
-				break;
+			// Nothing to do in default case.
+			break;
 		}
 	}
-		
-	//for (size_t i = 0; i < 1000; i += 4) {
-	//if (gpio_get_pin_value(QT1081_TOUCH_SENSOR_LEFT) == false) {
-	//++position;
-	//}
-	//else if (gpio_get_pin_value(QT1081_TOUCH_SENSOR_) == false) {
-	//gpio_set_gpio_pin(GPIO_PIN_EXAMPLE_2);
-	//}
-	//}
 }
